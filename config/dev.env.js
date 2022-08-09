657da0c686f99166789cbe3e0e167047f93c8645
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-09 16:10:05 +0800

    Delete config directory

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-const localEnv = require('./local.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  FIRE_BASE: localEnv["FIRE_BASE"],
-})
